bc5a16f3dcdc81cead439b364f98460755a9bb6b
zkatemor <ea.zababurina@gmail.com>
2020-12-18 20:48:07 +0300

    initial app

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.graph_objects as go
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
 
 app.layout = html.Div([
-    html.P("Color:"),
+    html.H2('Hello World'),
     dcc.Dropdown(
-        id="dropdown",
-        options=[
-            {'label': x, 'value': x}
-            for x in ['Gold', 'MediumTurquoise', 'LightGreen']
-        ],
-        value='Gold',
-        clearable=False,
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
     ),
-    dcc.Graph(id="graph"),
+    html.Div(id='display-value')
 ])
 
 
-@app.callback(
-    Output("graph", "figure"),
-    [Input("dropdown", "value")])
-def display_color(color):
-    fig = go.Figure(
-        data=go.Bar(y=[5, 1, 1], x=[4, 7, 10], marker_color=color))
-    return fig
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 
-app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
