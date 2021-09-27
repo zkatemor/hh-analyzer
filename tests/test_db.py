7eec7f4101952246386793217b2f6bb63a0e28f7
sabdam <sab.damirova@gmail.com>
2021-09-27 21:47:33 +0300

    test2

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,17 +1,42 @@
 import unittest
 
+import paramiko
 import sshtunnel
-
+import logging
 import scripts.analyze
 import analyze_test
 import pandas as pd
+from clickhouse_driver import Client
 
 cities = pd.read_csv('../settings/cities.csv')['city']
+
+mypkey = paramiko.RSAKey.from_private_key_file('../settings/aws_key.pem')
+SQL_HOSTNAME = 'localhost'
+SQL_USERNAME = 'default'
+SQL_PASSWORD = ''
+SQL_MAIN_DATABASE = 'hh_analyze'
+SQL_LOCAL_DATABASE = 'headhunter_salary'
+SQL_PORT = 9000
+
+SSH_HOST = 'ec2-3-20-222-181.us-east-2.compute.amazonaws.com'
+SSH_USER = 'ubuntu'
+SSH_PORT = 22
+
+def open_local_db():
+    """ open local database """
+    global CLIENT
+    CLIENT = Client(host='localhost',
+                    user=SQL_USERNAME,
+                    password=SQL_PASSWORD,
+                    database=SQL_LOCAL_DATABASE,
+                    port=SQL_PORT)
+    return CLIENT
 
 class TestApplication(unittest.TestCase):
 
     def test_db_max_salary(self):
-        testMaxSalary = analyze_test.experience_by_salary('CLIENT', 'Санкт-Петербург', cities)
+        client = open_local_db()
+        testMaxSalary = analyze_test.experience_by_salary(client, 'Санкт-Петербург', cities)
         self.assertEqual(testMaxSalary, 148597.67180460042, "Test cities error")
 
     # def open_ssh_tunnel(self):
